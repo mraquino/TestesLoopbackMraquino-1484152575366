91f0a7fdae013cd0a185a376a4f2d200b8f1a620
devopsdemo <mraquino@br.ibm.com>
2017-01-11 14:45:51 -0200

    teste

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,7 +4,9 @@
 // License text available at https://opensource.org/licenses/MIT
 'use strict';
 
-module.exports = function enableAuthentication(server) {
-  // enable authentication
-  server.enableAuth();
+module.exports = function(server) {
+  // Install a `/` route that returns server status
+  var router = server.loopback.Router();
+  router.get('/status', server.loopback.status());
+  server.use(router);
 };
